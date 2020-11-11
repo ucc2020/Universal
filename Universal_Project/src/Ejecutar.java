c739ab301275f45f9b30a507b4962daca44995b1
christianruiz <christianruiz@localhost>
2020-11-10 20:31:26 -0500

    metodo pedido

--- Universal_Project/src/Ejecutar.java
+++ Universal_Project/src/Ejecutar.java
@@ -1,10 +1,54 @@
 
+import javax.swing.JOptionPane;
+
+
+
+
 public class Ejecutar {
 
   
     public static void main(String[] args) {
        
-        System.out.println("hello world");
+        
+       
+        
+        
+        
+        
+        int opcion =0;
+        int opc =0;
+        
+        do{
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+            "1.Agregar pedido\n"
+            + "2.Eliminar Pedido\n"
+            + "3.Mostrar pedidos realizados\n"
+            + "4.Salir\n"
+            + "Que desea hacer?"));
+            switch(opcion){
+                
+                case 1:
+                       
+                    break;
+                    
+                case 2:
+                    
+                    break;
+                    
+                case 3:
+                    
+                    break;
+                    
+               
+                    
+                  
+            }
+                 
+        }while(opcion !=4);
+        
+        
+        
+      
     }
     
 }
